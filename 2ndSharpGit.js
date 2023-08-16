622f124490203f2d379980fa06b65e065bae5ade
MAYURI MORE <122468060+Mayuri-More263@users.noreply.github.com>
2023-08-16 19:29:43 +0530

    Update 2ndSharpGit.js

--- 2ndSharpGit.js
+++ 2ndSharpGit.js
@@ -1,1 +1,9 @@
 console.log("I LOVE GIT");
+var li = document.querySelectorAll("li");
+li[2].style.backgroundColor = "green";
+titles[1].style.fontweight = "bold";
+titles[2].style.fontweight = "bold";
+items[0].style.fontweight = "bold";
+items[1].style.fontweight = "bold";
+items[2].style.fontweight = "bold";
+items[3].style.fontweight = "bold";
